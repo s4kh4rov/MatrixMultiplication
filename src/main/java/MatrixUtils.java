a7ecc7b284c1f2d3fb79f1b1216d726378e84c0f
Alexander Sakharov <sakharov487@mail.ru>
2021-03-10 18:10:08 +0300

    Added Javadoc

--- src/main/java/MatrixUtils.java
+++ src/main/java/MatrixUtils.java
@@ -1,6 +1,14 @@
 import java.util.Random;
 
 public class MatrixUtils {
+    /**
+     * Creates a matrix of a given size filled with random values
+     *
+     * @param rows         the number of rows in the matrix
+     * @param cols         the number of columns in the matrix
+     * @param numbersLimit the maximum random number that can be the value of the matrix cell
+     * @return a new matrix filled with numbers
+     */
     public static double[][] createRandomMatrix(int rows, int cols, int numbersLimit) {
         double[][] arr = new double[rows][cols];
         Random random = new Random();
@@ -12,6 +20,13 @@ public class MatrixUtils {
         return arr;
     }
 
+    /**
+     * Multiplies matrices in single-threaded mode
+     *
+     * @param m1 first matrix
+     * @param m2 second matrix
+     * @return matrix resulting from multiplication
+     */
     public static double[][] multiplyMatrix(double[][] m1, double[][] m2) {
         double[][] mResult = new double[m1.length][m2[0].length];
         for (int i = 0; i < m1.length; i++) {
--- src/main/java/MultithreadMatrixMultiplication.java
+++ src/main/java/MultithreadMatrixMultiplication.java
@@ -2,9 +2,21 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MultithreadMatrixMultiplication {
+    /**
+     * Number of threads
+     */
     private int threadsCount;
+    /**
+     * First matrix to multiply
+     */
     private double[][] firstMatrix;
+    /**
+     * Second matrix to multiply
+     */
     private double[][] secondMatrix;
+    /**
+     * Resulting matrix
+     */
     private volatile double[][] resultMatrix;
 
     public MultithreadMatrixMultiplication(double[][] firstMatrix, double[][] secondMatrix) {
@@ -14,6 +26,11 @@ public class MultithreadMatrixMultiplication {
         this.resultMatrix = new double[firstMatrix.length][secondMatrix[0].length];
     }
 
+    /**
+     * Performs multiplication of two matrices in multi-threaded mode
+     *
+     * @return resulting matrix
+     */
     public double[][] matrixMultiplication() {
         ExecutorService service = Executors.newFixedThreadPool(threadsCount);
         for (int i = 0; i < firstMatrix.length; i++) {
@@ -24,12 +41,18 @@ public class MultithreadMatrixMultiplication {
     }
 
     private class MultiplicationThread implements Runnable {
+        /**
+         * Line number whose value will be calculated during the execution of the run method
+         */
         private int stringIndex;
 
         public MultiplicationThread(int stringIndex) {
             this.stringIndex = stringIndex;
         }
 
+        /**
+         * Calculates the values of the row with the stringIndex
+         */
         @Override
         public void run() {
             for (int i = 0; i < firstMatrix.length; i++) {
